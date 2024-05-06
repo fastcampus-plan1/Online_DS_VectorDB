8ef728d6652fb1e40c076360ae8f1142baa3ccc6
hooman34 <30697676+hooman34@users.noreply.github.com>
2024-05-06 23:01:25 +0900

    Add fashion rec module

---
+++ yolo_utils.py
@@ -0,0 +1,72 @@
+from PIL import Image
+from transformers import YolosFeatureExtractor, YolosForObjectDetection
+import torch
+import matplotlib.pyplot as plt
+from torchvision.transforms import ToTensor, ToPILImage
+
+# https://github.com/valentinafeve/fine_tunning_YOLOS_for_fashion/blob/main/Using_a_fine_tunned_YOLOS_model.ipynb
+
+MODEL_NAME = "valentinafeve/yolos-fashionpedia"
+
+# This is the order of the categories list. NO NOT CHANGE. Just for visualization purposes
+cats = ['shirt, blouse', 'top, t-shirt, sweatshirt', 'sweater', 'cardigan', 'jacket', 'vest', 'pants', 'shorts', 'skirt', 'coat', 'dress', 'jumpsuit', 'cape', 'glasses', 'hat', 'headband, head covering, hair accessory', 'tie', 'glove', 'watch', 'belt', 'leg warmer', 'tights, stockings', 'sock', 'shoe', 'bag, wallet', 'scarf', 'umbrella', 'hood', 'collar', 'lapel', 'epaulette', 'sleeve', 'pocket', 'neckline', 'buckle', 'zipper', 'applique', 'bead', 'bow', 'flower', 'fringe', 'ribbon', 'rivet', 'ruffle', 'sequin', 'tassel']
+
+# Random colors used for visualization
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+def fix_channels(t):
+    """
+    Some images may have 4 channels (transparent images) or just 1 channel (black and white images), in order to let the images have only 3 channels. I am going to remove the fourth channel in transparent images and stack the single channel in back and white images.
+    :param t: Tensor-like image
+    :return: Tensor-like image with three channels
+    """
+    if len(t.shape) == 2:
+        return ToPILImage()(torch.stack([t for i in (0, 0, 0)]))
+    if t.shape[0] == 4:
+        return ToPILImage()(t[:3])
+    if t.shape[0] == 1:
+        return ToPILImage()(torch.stack([t[0] for i in (0, 0, 0)]))
+    return ToPILImage()(t)
+
+def idx_to_text(i):
+    return cats[i]
+
+# for output bounding box post-processing
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+def plot_results(pil_img, prob, boxes):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    colors = COLORS * 100
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), colors):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        cl = p.argmax()
+        ax.text(xmin, ymin, idx_to_text(cl), fontsize=10,
+                bbox=dict(facecolor=c, alpha=0.8))
+    plt.axis('off')
+    plt.show()
+    # plt.savefig("image.png")
+
+def visualize_predictions(image, outputs, threshold=0.8):
+    # keep only predictions with confidence >= threshold
+    probas = outputs.logits.softmax(-1)[0, :, :-1]
+    keep = probas.max(-1).values > threshold
+
+    # convert predicted boxes from [0; 1] to image scales
+    bboxes_scaled = rescale_bboxes(outputs.pred_boxes[0, keep].cpu(), image.size)
+
+    # plot results
+    plot_results(image, probas[keep], bboxes_scaled)
